daa301a263630cfc5f29cba25536c67d1576af58
dkohorn <dkohorn@silverfallscapital.com>
2025-06-06 10:05:32 -0700

    Update esp_zigbee_gateway.c

--- Zigbee_Coordinator/main/esp_zigbee_gateway.c
+++ Zigbee_Coordinator/main/esp_zigbee_gateway.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/timers.h"
 #include "driver/usb_serial_jtag.h"
 #include "esp_coexist.h"
 #include "esp_check.h"
@@ -28,10 +29,20 @@ static const bool DO_I2C_REPORTING = true; //! used for turning on/off RPi readi
 #define I2C_SLAVE_NUM I2C_NUM_0
 #define I2C_BUF_LEN 128
 #define RTT_BYTE_IDENTIFIER 0xB2
+#define DISCONNECT_BYTE_IDENTIFIER 0xA1
 
-//Sensor setup
+//Vibration Sensor data collection
 #define PIEZO_ADC_CHANNEL    ADC_CHANNEL_2  // GPIO2
 #define PIEZO_ADC_UNIT          ADC_UNIT_1
+#define SENSOR_DATA_COMMAND_REQ_ID          0x0002
+#define SENSOR_READING_DELAY 10000 
+static uint16_t sensor_value = 0; 
+adc_oneshot_unit_handle_t adc_handle;
+static bool created_sensor_task = false; // Prevent multiple task creation, currently starts in ANNCE signal when new device joins network
+
+//Timeout
+#define TIMEOUT_CYCLE_TIME SENSOR_READING_DELAY + 2000
+static TimerHandle_t timeout_timer = NULL;
 
 //Cluster and Endpoints
 #define ESP_ZB_ROUTER_ENDPOINT        0x02 //Not necessary for endpoints on different devices to differ, however it allows for easier tracking
@@ -42,25 +53,14 @@ static const bool DO_I2C_REPORTING = true; //! used for turning on/off RPi readi
 #define CUSTOM_PAN_ID               0x1234 
 static const esp_zb_ieee_addr_t CUSTOM_EXTENDED_PAN_ID = {0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0};
 
-//Sensor reading
-#define SENSOR_DATA_COMMAND_REQ_ID          0x0002
-#define SENSOR_READING_DELAY 5000 
-static uint16_t sensor_value = 0; 
-adc_oneshot_unit_handle_t adc_handle;
-static bool created_sensor_task = false; // Prevent multiple task creation
 
-//Connection timeout
-#define CONNECTION_TIMEOUT SENSOR_READING_DELAY + 1000 //Always 1 second more than the read delay
-static bool data_is_receiving = false;
-static bool started_timeout_task = false; // Prevent multiple task creation
-
-//LED: 1 = receiving data
+//LED: 1 = connected and on, 0 = disconnected and off
 #define LED_GPIO_PIN GPIO_NUM_5
 
 //Network state
-static uint8_t network_time_open = 255;
-static uint16_t router_short_address = 0;                                       //Obtained in ANNCE
-static esp_zb_ieee_addr_t router_ieee_address = {0, 0, 0, 0, 0, 0, 0, 0,};
+static bool network_initally_opened = false; //One time flag for the keep_network_open() call on device reboot
+static uint8_t network_time_open = 255; //In seconds
+static uint16_t router_short_address = 0;    //Obtained in ANNCE
 
 //RTT 
 static uint64_t previous_send_time = 0;
@@ -72,14 +72,7 @@ static const char *TAG = "ESP_ZB_COORDINATOR";
 /*
     CALLBACKS
 */
-static void bind_cb(esp_zb_zdp_status_t zdo_status, void *user_ctx) {
-    if (zdo_status == ESP_ZB_ZDP_STATUS_SUCCESS) {
-        ESP_LOGI("BIND", "Bind successful");
-    } else {
-        ESP_LOGE("BIND", "Bind failed with status: 0x%02x", zdo_status);
-    }
-}
-
+//Puts commission on the stack in a context its normally not possible
 static void bdb_start_top_level_commissioning_cb(uint8_t mode_mask) {
     ESP_RETURN_ON_FALSE(esp_zb_bdb_start_top_level_commissioning(mode_mask) == ESP_OK, , TAG, "Failed to start Zigbee bdb commissioning");
 }
@@ -88,10 +81,20 @@ static void bdb_start_top_level_commissioning_cb(uint8_t mode_mask) {
 /*
     TASKS
 */
+//Constant reopenning of network for developememnt and testing purpose only
+void keep_network_live(void *arg) {
+    while (1) {
+        esp_zb_bdb_open_network(network_time_open);
+        vTaskDelay(pdMS_TO_TICKS(network_time_open * 1000));
+    }
+}
+
 //Read and print adc value every 3 seconds
 void adc_read_task(void *arg)
 {
     while (1) {
+        vTaskDelay(pdMS_TO_TICKS(SENSOR_READING_DELAY));
+
         int raw_data;
         if (adc_oneshot_read(adc_handle, PIEZO_ADC_CHANNEL, &raw_data) == ESP_OK) {
             sensor_value = (uint16_t) raw_data;
@@ -125,46 +128,37 @@ void adc_read_task(void *arg)
         } else {
             ESP_LOGE("ADC", "Failed to read ADC");
         }
-        vTaskDelay(pdMS_TO_TICKS(SENSOR_READING_DELAY)); //delay between reading sends
-    }
-}
-
-//Used for LED signalling of router disconnecting for unforseeable reasons 
-static void timeout_task(void *pvParameters) {
-    while (1) {
-        vTaskDelay(pdMS_TO_TICKS(CONNECTION_TIMEOUT));
-
-        if (data_is_receiving) {
-            data_is_receiving = false; // will be reset to true by handler if data came in, otherwise will alert to connection lost
-        }
-        else {
-            ESP_LOGE(TAG, "Possible connection lost due to timeout (missed data ACK)");
-            gpio_set_level(LED_GPIO_PIN, 0);  // LED off to indicate failure
-        }
+        
     }
 }
 
 
+
+//Happens when an ACK is missed and the timer expires
+void timer_callback(TimerHandle_t xTimer) {
+    gpio_set_level(LED_GPIO_PIN, 0);
+
+    if (DO_I2C_REPORTING) { //!Will be disabled if DO_I2C_REPORTING = false
+        uint8_t buffer[9];
+        
+        buffer[0] = DISCONNECT_BYTE_IDENTIFIER;  // Identifier
+        for (int i = 0; i < 8; i++) {
+            buffer[i + 1] = (esp_timer_get_time() >> (56 - i * 8)) & 0xFF;
+        } 
+
+        // Write to internal ring buffer (I2C slave will return this on RPi request)
+        i2c_slave_write_buffer(I2C_SLAVE_NUM, buffer, sizeof(buffer), pdMS_TO_TICKS(10));
+    }
+}
 
 
 /*
     HANDLERS
 */
 static esp_err_t zb_custom_cmd_handler(const esp_zb_zcl_custom_cluster_command_message_t *message) {
+    xTimerReset(timeout_timer, 0);
+    gpio_set_level(LED_GPIO_PIN, 1);
 
-    //Ensure flag and LED are set only when strictly necessary
-    if (!data_is_receiving) {
-        data_is_receiving = true;
-
-        //Turn on to indicate data is coming in
-        gpio_set_level(LED_GPIO_PIN, 1);
-    }
-
-    //Begin timeout when first data is acknowledged
-    if (!started_timeout_task) {
-        started_timeout_task = true;
-        xTaskCreate(timeout_task, "timeout_task", 4096, NULL, 5, NULL);
-    }
 
     ESP_RETURN_ON_FALSE(message, ESP_FAIL, TAG, "Empty message");
     ESP_RETURN_ON_FALSE(message->info.status == ESP_ZB_ZCL_STATUS_SUCCESS, ESP_ERR_INVALID_ARG, TAG, "Received message: error status(%d)", message->info.status);
@@ -216,7 +210,10 @@ void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct) {
                 ESP_LOGI(TAG, "Start network formation");
                 esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_FORMATION);
             } else {
-                esp_zb_bdb_open_network(network_time_open);
+                if (!network_initally_opened) {
+                    xTaskCreate(keep_network_live, "keep_network_live", 4096, NULL, 5, NULL);
+                    network_initally_opened = true;
+                }
                 ESP_LOGI(TAG, "Device rebooted");
 
             }
@@ -249,23 +246,12 @@ void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct) {
 
         //Store device address to send zcl data or other requests
         router_short_address = dev_annce_params->device_short_addr;
-        esp_zb_ieee_address_by_short(router_short_address, router_ieee_address);
-
-        //Send bind request to the receiver
-        esp_zb_zdo_bind_req_param_t bind_req = {
-            .src_endp = ESP_ZB_COORDINATOR_ENDPOINT,
-            .cluster_id = CUSTOM_CLUSTER_ID,
-            .dst_addr_mode = ESP_ZB_ZDO_BIND_DST_ADDR_MODE_64_BIT_EXTENDED,
-            .dst_endp = ESP_ZB_ROUTER_ENDPOINT,
-            .req_dst_addr = esp_zb_get_short_address(), // I think this address is the coordinator because the bind is on itself and the the dst_addr is the other end of the bind
-        };
-        esp_zb_get_long_address(bind_req.src_address); // set src_address parameter
-        memcpy(bind_req.dst_address_u.addr_long, router_ieee_address, sizeof(esp_zb_ieee_addr_t));
-        esp_zb_zdo_device_bind_req(&bind_req, bind_cb, NULL);
 
         if (!created_sensor_task) {
             created_sensor_task = true;
             xTaskCreate(adc_read_task, "adc_read_task", 2048, NULL, 5, NULL);
+            timeout_timer = xTimerCreate("timeout_timer", pdMS_TO_TICKS(TIMEOUT_CYCLE_TIME), pdFALSE, NULL, timer_callback);
+            xTimerStart(timeout_timer, 10);
         }
 
         break;
@@ -275,16 +261,10 @@ void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct) {
                 esp_zb_ieee_addr_t extended_pan_id;
                 esp_zb_get_extended_pan_id(extended_pan_id);
 
-                ESP_LOGI(TAG, "Network(Pan ID: 0x%04hx | Extended: %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x | Channel: %d | Short Addr: 0x%04hx) is open for %d seconds", 
-                    esp_zb_get_pan_id(),
-                    extended_pan_id[7], extended_pan_id[6], extended_pan_id[5], extended_pan_id[4],
-                    extended_pan_id[3], extended_pan_id[2], extended_pan_id[1], extended_pan_id[0],
-                    esp_zb_get_current_channel(), 
-                    esp_zb_get_short_address(),
-                    *(uint8_t *)esp_zb_app_signal_get_params(p_sg_p));
+                ESP_LOGI(TAG, "Network(0x%04hx) is open for %d seconds", esp_zb_get_pan_id(), *(uint8_t *)esp_zb_app_signal_get_params(p_sg_p));
 
             } else {
-                ESP_LOGW(TAG, "Network(0x%04hx) closed, devices joining not allowed.", esp_zb_get_pan_id());
+                //ESP_LOGW(TAG, "Network(0x%04hx) closed, devices joining not allowed.", esp_zb_get_pan_id());
             }
         }
         break;
@@ -292,8 +272,12 @@ void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct) {
         ESP_LOGI(TAG, "Production configuration is %s", err_status == ESP_OK ? "ready" : "not present");
         esp_zb_set_node_descriptor_manufacturer_code(ESP_MANUFACTURER_CODE);
         break;
-    case ESP_ZB_ZDO_SIGNAL_LEAVE_INDICATION:
-        ESP_LOGE(TAG, "Child device has left the network by request");
+    case ESP_ZB_NLME_STATUS_INDICATION:
+        ESP_LOGE(TAG, "Connection severed from loss of a link"); //Likely due to a range issue or random interference
+        break;
+    //Router out of range or lost power
+    case ESP_ZB_ZDO_DEVICE_UNAVAILABLE:
+        ESP_LOGE(TAG, "Router no longer available"); //Router out of range or lost power
         break;
     default:
         ESP_LOGI(TAG, "ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type,
@@ -348,7 +332,6 @@ void init_led()
     };
     gpio_config(&io_conf);
 
-    //Init to 0 and turn on only when 1st piece of data received
     gpio_set_level(LED_GPIO_PIN, 0); 
 
 }
